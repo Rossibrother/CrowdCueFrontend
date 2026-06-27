ec194200bff84a468c8a248d6c800572810f625c
Noah Roßnagel <noah.rossnagel@gmx.de>
2026-06-27 13:13:51 +0200

    added spinner

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,12 +3,13 @@ import { Router } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { QueueService } from '../service/queue.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, MatCardModule],
+  imports: [MatButtonModule, MatIconModule, MatCardModule, MatProgressSpinnerModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
@@ -16,10 +17,16 @@ export class DashboardComponent {
   private queueService = inject(QueueService);
   private router = inject(Router);
 
+  isCreating = false;
+
   createQueue() {
+    this.isCreating = true;
     this.queueService.createQueue().subscribe({
       next: (queue) => this.router.navigate(['/dj-view', queue.id]),
-      error: (err) => console.error('Fehler beim Erstellen der Queue:', err)
+      error: (err) => {
+        console.error('Fehler beim Erstellen der Queue:', err);
+        this.isCreating = false;
+      }
     });
   }
 }
