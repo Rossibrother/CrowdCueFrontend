12a478ececc00091dcc6cec2c866defb382e7456
Noah Roßnagel <noah.rossnagel@gmx.de>
2026-06-27 02:26:31 +0200

    fix(environment.production.ts): update wsUrl to use wss protocol for production

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://crowdcue-production.up.railway.app',
-  wsUrl: 'ws://crowdcue-production.up.railway.app/ws'
+  wsUrl: 'wss://crowdcue-production.up.railway.app/ws'
 };
